947924303fa50d6f9e5643f8f636ccc174da72e3
Michel Hognerud <leyou.m@gmail.com>
2014-04-23 14:46:45 +0800

    fixes

--- Controllers/ContactController.php
+++ Controllers/ContactController.php
@@ -6,11 +6,11 @@ class ContactController extends \Asgard\Core\Controller {
 	@Route('contact')
 	*/
 	public function indexAction($request) {
-		$this->form = new Form;
-		$this->form->name = new TextField(array('validation'=>'required'));
-		$this->form->email = new TextField(array('validation'=>array('required', 'email')));
-		$this->form->message = new TextField(array('validation'=>'required'));
-		$this->form->captcha = new CaptchaField;
+		$this->form = new \Asgard\Form\Form;
+		$this->form->name = new \Asgard\Form\Fields\TextField(array('validation'=>'required'));
+		$this->form->email = new \Asgard\Form\Fields\TextField(array('validation'=>array('required', 'email')));
+		$this->form->message = new \Asgard\Form\Fields\TextField(array('validation'=>'required'));
+		$this->form->captcha = new \Asgard\Form\Fields\CaptchaField;
 
 		if($this->form->isSent()) {
 			if($this->form->isValid()) {
@@ -20,7 +20,7 @@ class ContactController extends \Asgard\Core\Controller {
 						continue;
 					$txt .= $field->label().': '.$field->getValue()."\n";
 				}
-				$email = Email::create(Value::val('email'), $this->form->email->getValue(), 'Contact', $txt);
+				$email = \Asgard\Utils\Email::create(Value::val('email'), $this->form->email->getValue(), 'Contact', $txt);
 				// if($this->form->photo->getValue()) {
 				// 	$name = Tools::array_get($this->form->photo->getValue(), 'name');
 				// 	$path = Tools::array_get($this->form->photo->getValue(), 'path');
